f856a857ec705d6c11105e594dfa2decc01e326e
Daniel Macharia <danielhuios@gmail.com>
2022-10-28 00:46:17 +0300

    completed inheritance lab

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,18 @@
-class Student 
+require_relative "./user.rb"
+require "pry"
+class Student < User
+    attr_accessor :knowledge
 
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def learn(str)
+        @knowledge << str
+    end
+  
 end
+
+daniel  = Student.new
+
+# binding.pry
